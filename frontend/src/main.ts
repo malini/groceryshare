bc0341f9f3888825eda9600b23e18e31f8b61ed1
malini <malini@groceryshare.ca>
2025-01-03 22:48:47 -0500

    remove old app.vue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-//import App from './App.vue'
 import MainApp from './MainApp.vue'
 import router from './router'
 
