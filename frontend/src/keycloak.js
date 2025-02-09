30734e297da1592f303359f29088746335770492
malini <malini@groceryshare.ca>
2025-02-08 21:13:11 -0500

    move backend apis to env var

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from "keycloak-js";
 
 // TODO: dev and prod versions
 const keycloak = new Keycloak({
-  url: "http://localhost:9999",
+  url: import.meta.env.VITE_KEYCLOAK_URL,
   realm: "groceryshare",
   clientId: "groceryshare-frontend",
   checkLoginIframe: false,
